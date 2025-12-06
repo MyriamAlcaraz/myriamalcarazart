f1c63edba23062d6ae9bd58f563c90a0454e679f
MyriamAlcaraz <myriamhotmail@hotmail.com>
2025-12-07 00:37:48 +0100

    pantalla blanca

--- components/vite.config.js
+++ components/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Clave configurada correctamente
     'process.env.API_KEY': JSON.stringify("AIzaSyC0SA_dMAsU3-MPbiIAEiVIUE5LSpyKRMk")
   }
 })
