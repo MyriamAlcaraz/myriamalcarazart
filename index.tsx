b3742dc69e8b59c421f0dcdba1bdace3fc78c3ef
MyriamAlcaraz <myriamhotmail@hotmail.com>
2025-12-06 21:20:46 +0100

    error en nombre MainApp

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './MainApp';
+import App from './App';
 import './index.css'; // <--- ESTA ES LA LÍNEA QUE ENCIENDE EL DISEÑO
 
 const rootElement = document.getElementById('root');
