da2892b4ec82ee5465c4f11080ed8c137d827b1f
MyriamAlcaraz <myriamhotmail@hotmail.com>
2026-02-28 20:33:25 +0100

    Monetag: tag en index.html raíz + sw.js zoneId 10666566

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10666504
+    "zoneId": 10666566
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
